df8aa5e2094200bfa3eb011c92a048898d348200
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-02-21 20:28:40 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -6,6 +6,7 @@ class TypewiseTest(unittest.TestCase):
   def test_infers_breach_as_per_limits(self):
     self.assertTrue(typewise_alert.infer_breach(20, 50, 100) == 'TOO_LOW')
     self.assertTrue(typewise_alert.infer_breach(120, 50, 100) == 'TOO_HIGH')
+    self.assertTrue(typewise_alert.infer_breach(99, 50, 100) == 'NORMAL')
 
 
 if __name__ == '__main__':
