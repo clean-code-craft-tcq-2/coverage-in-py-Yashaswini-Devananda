b4cfe4512e8bb327124fcda803581b5d98e39577
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-02-22 20:35:32 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -10,6 +10,13 @@ class TypewiseTest(unittest.TestCase):
     self.assertTrue(typewise_alert.infer_breach(99, 50, 100) == 'NORMAL')
     self.assertTrue(typewise_alert.infer_breach(50, 50, 100) == 'NORMAL')
     self.assertTrue(typewise_alert.infer_breach(100, 50, 100) == 'NORMAL')
+    
+    self.assertTrue(typewise_alert.classify_temperature_breach(PASSIVE_COOLING, -1) == 'TOO_LOW')
+    self.assertTrue(typewise_alert.classify_temperature_breach(PASSIVE_COOLING, 36) == 'TOO_HIGH')
+    self.assertTrue(typewise_alert.classify_temperature_breach(PASSIVE_COOLING, 0) == 'NORMAL')
+    self.assertTrue(typewise_alert.classify_temperature_breach(PASSIVE_COOLING, 35) == 'NORMAL')
+    self.assertTrue(typewise_alert.classify_temperature_breach(PASSIVE_COOLING, 1) == 'NORMAL')
+    self.assertTrue(typewise_alert.classify_temperature_breach(PASSIVE_COOLING, 34) == 'NORMAL')
 
 
 if __name__ == '__main__':
