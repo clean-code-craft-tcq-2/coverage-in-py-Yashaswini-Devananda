8171256c7b9b3216d07e5f62efc7377bc9c90469
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-02-24 15:37:13 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -8,7 +8,7 @@ class TypewiseTest(unittest.TestCase):
     if alertTarget == typewise_alert.AlertTarget.TO_CONTROLLER:
         message = f'{typewise_alert.alerter_ref_strings[typewise_alert.AlertTarget.TO_CONTROLLER]}, {typewise_alert.infer_breach(breachType)}'
     elif breachType!=typewise_alert.BreachType.NORMAL and alertTarget == typewise_alert.AlertTarget.TO_EMAIL:
-        message = f"{typewise_alert.alerter_ref_strings[AlertTarget.TO_EMAIL]}{typewise_alert.breach_ref_strings[breachType]}"
+        message = f"{typewise_alert.alerter_ref_strings[typewise_alert.AlertTarget.TO_EMAIL]}{typewise_alert.breach_ref_strings[breachType]}"
     return message
   
   def test_infers_breach_as_per_limits(self):
@@ -17,7 +17,7 @@ class TypewiseTest(unittest.TestCase):
             typewise_alert.battery["CoolingType"] = coolingType
 
             temperatureInC = typewise_alert.cooling_type_list[coolingType].lowerlimit-0.1
-            self.assertTrue(typewise_alert.check_and_alert(alertTarget, typewise_alert.battery, temperatureInC) == generate_expected_message(alertTarget, typewise_alert.BreachType.TOO_LOW))
+            self.assertTrue(typewise_alert.check_and_alert(alertTarget, typewise_alert.battery, temperatureInC) == self.generate_expected_message(alertTarget, typewise_alert.BreachType.TOO_LOW))
 
 
 if __name__ == '__main__':
