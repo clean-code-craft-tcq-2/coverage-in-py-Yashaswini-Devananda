1947b4d76dc4594c0ebef2266dfb51b2c8dde6f5
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-02-24 15:32:13 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -5,10 +5,10 @@ import typewise_alert
 class TypewiseTest(unittest.TestCase):
   def generate_expected_message(alertTarget, breachType):
     message = ""
-    if alertTarget == AlertTarget.TO_CONTROLLER:
-        message = f'{alerter_ref_strings[AlertTarget.TO_CONTROLLER]}, {infer_breach(breachType)}'
-    elif breachType!=BreachType.NORMAL and alertTarget == AlertTarget.TO_EMAIL:
-        message = f"{alerter_ref_strings[AlertTarget.TO_EMAIL]}{breach_ref_strings[breachType]}"
+    if alertTarget == typewise_alert.AlertTarget.TO_CONTROLLER:
+        message = f'{typewise_alert.alerter_ref_strings[typewise_alert.AlertTarget.TO_CONTROLLER]}, {typewise_alert.infer_breach(breachType)}'
+    elif breachType!=typewise_alert.BreachType.NORMAL and alertTarget == typewise_alert.AlertTarget.TO_EMAIL:
+        message = f"{typewise_alert.alerter_ref_strings[AlertTarget.TO_EMAIL]}{typewise_alert.breach_ref_strings[breachType]}"
     return message
   
   def test_infers_breach_as_per_limits(self):
