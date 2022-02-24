4bd69e0efe09b55f648d25034262e054e6268771
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-02-24 17:31:25 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -48,6 +48,8 @@ class TypewiseTest(unittest.TestCase):
             for test_case in test_cases:
               actual_message = typewise_alert.check_and_alert(alertTarget, typewise_alert.battery, test_case["temperatureInC"])
               self.assertTrue(self.generate_expected_message(alertTarget, test_case["BreachType"]) == actual_message)
+              print (actual_message)
+              print (self.generate_expected_message(alertTarget, test_case["BreachType"]))
       
                             
 
