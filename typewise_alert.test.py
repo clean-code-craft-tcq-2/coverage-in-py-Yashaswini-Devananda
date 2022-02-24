f353ebc8d7ea99339a4c4a8f8c6044f661b8a2f6
Yashaswini-Devananda <90909294+Yashaswini-Devananda@users.noreply.github.com>
2022-02-24 17:38:05 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -1,17 +1,6 @@
 import unittest
 import typewise_alert
 
-coolingType = 0
-  
-test_cases = [
-              {"temperatureInC" : typewise_alert.cooling_type_list[coolingType].lowerlimit-0.1, "BreachType": typewise_alert.BreachType.TOO_LOW},
-              {"temperatureInC" : typewise_alert.cooling_type_list[coolingType].lowerlimit+0.1, "BreachType": typewise_alert.BreachType.NORMAL},
-              {"temperatureInC" : typewise_alert.cooling_type_list[coolingType].lowerlimit, "BreachType": typewise_alert.BreachType.NORMAL},
-              {"temperatureInC" : typewise_alert.cooling_type_list[coolingType].upperlimit-0.1, "BreachType": typewise_alert.BreachType.NORMAL},
-              {"temperatureInC" : typewise_alert.cooling_type_list[coolingType].upperlimit+0.1, "BreachType": typewise_alert.BreachType.TOO_HIGH},
-              {"temperatureInC" : typewise_alert.cooling_type_list[coolingType].upperlimit, "BreachType": typewise_alert.BreachType.NORMAL},
-             ]
-
 class TypewiseTest(unittest.TestCase):
   def generate_controller_message(self,alertTarget, breachType):
     message = f'{typewise_alert.alerter_ref_strings[typewise_alert.AlertTarget.TO_CONTROLLER]}, {typewise_alert.infer_breach(breachType)}'
@@ -40,11 +29,17 @@ class TypewiseTest(unittest.TestCase):
 #     self.assertTrue(self.generate_expected_message(alertTarget, typewise_alert.BreachType.TOO_LOW)==actual_message)
     
   def test_infers_breach_as_per_limits(self):
-    
     for alertTarget in range (typewise_alert.AlertTarget.AlertTarget_count):
         for coolingType in range (len(typewise_alert.CoolingType)):
             typewise_alert.battery["CoolingType"] = coolingType
-           
+            test_cases = [
+              {"temperatureInC" : typewise_alert.cooling_type_list[coolingType].lowerlimit-0.1, "BreachType": typewise_alert.BreachType.TOO_LOW},
+              {"temperatureInC" : typewise_alert.cooling_type_list[coolingType].lowerlimit+0.1, "BreachType": typewise_alert.BreachType.NORMAL},
+              {"temperatureInC" : typewise_alert.cooling_type_list[coolingType].lowerlimit, "BreachType": typewise_alert.BreachType.NORMAL},
+              {"temperatureInC" : typewise_alert.cooling_type_list[coolingType].upperlimit-0.1, "BreachType": typewise_alert.BreachType.NORMAL},
+              {"temperatureInC" : typewise_alert.cooling_type_list[coolingType].upperlimit+0.1, "BreachType": typewise_alert.BreachType.TOO_HIGH},
+              {"temperatureInC" : typewise_alert.cooling_type_list[coolingType].upperlimit, "BreachType": typewise_alert.BreachType.NORMAL},
+             ]
             for test_case in test_cases:
               actual_message = typewise_alert.check_and_alert(alertTarget, typewise_alert.battery, test_case["temperatureInC"])
               self.assertTrue(self.generate_expected_message(alertTarget, test_case["BreachType"]) == actual_message)
